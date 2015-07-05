76878f96ebe1203ec49767846637e2c9d93c9f38
Sebastian Metzger <sebastian.t.metzger@gmail.com>
2015-07-05 15:59:08 +0200

    feat(advanced to-do)

--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,61 @@
-var app = angular.module('ToDoApp', []);
-app.controller('ToDoCtrl', function($scope) {
+var app = angular.module('ToDoApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider.
+  when('/', {
+    controller: 'ToDoCtrl'
+  }).
+  when('/archive', {
+    controller: 'ToDoCtrl'
+  }).
+  otherwise({
+    redirectTo: '/'
+  });
+});
+
+app.run(function($route){
+  //
+});
+
+app.filter('byArchived', function() {
+    return function(tasks,archive) {
+      tasks = tasks.filter(function(task){
+        if(archive){
+          return task.archived;
+        } else {
+          return !task.archived;
+        }
+      });
+      return tasks;
+    }
+  });
+
+app.controller('ToDoCtrl', function($scope, $rootScope, $location) {
   $scope.tasks = [];
   $scope.newTaskName = '';
+  $scope.archive = false;
+
+  $rootScope.$on("$routeChangeSuccess", function(event, next, current) {
+    if(next.$$route.originalPath === '/archive'){
+      $scope.archive = true;
+    } else {
+      $scope.archive = false;
+    }
+  });
 
   $scope.createNewTask = function() {
     $scope.tasks.push({
       name: $scope.newTaskName,
-      done: false
+      done: false,
+      archived: false
     });
     $scope.newTaskName = '';
   };
 
-  $scope.deleteDoneTasks = function() {
-    $scope.tasks = $scope.tasks.filter(function(task){
-      return !task.done;
+  $scope.archiveDoneTasks = function() {
+    $scope.tasks = $scope.tasks.map(function(task) {
+      if(task.done) task.archived = true;
+      return task;
     });
   };
 });
