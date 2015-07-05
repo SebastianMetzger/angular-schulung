e55647753fe0e166f700a40fc060c0bc99f06b76
Sebastian Metzger <sebastian.t.metzger@gmail.com>
2015-07-05 20:05:59 +0200

    feat(advanced task list)

--- src/scripts.js
+++ src/scripts.js
@@ -3,47 +3,68 @@ var app = angular.module('ToDoApp', ['ngRoute']);
 app.config(function($routeProvider) {
   $routeProvider.
   when('/', {
-    controller: 'ToDoCtrl'
+    controller: 'ToDoCtrl',
+    templateUrl: 'views/todo.html'
   }).
   when('/archive', {
-    controller: 'ToDoCtrl'
+    controller: 'ArchiveCtrl',
+    templateUrl: 'views/archive.html'
   }).
   otherwise({
     redirectTo: '/'
   });
 });
 
-app.run(function($route){
-  //
+// app.filter('byArchived', function() {
+//     return function(tasks,archive) {
+//       tasks = tasks.filter(function(task){
+//         if(archive){
+//           return task.archived;
+//         } else {
+//           return !task.archived;
+//         }
+//       });
+//       return tasks;
+//     }
+//   });
+
+app.factory('Tasks', function() {
+  var LOCAL_STORAGE_KEY = 'TO_DO_APP.TASKS';
+  var _tasks = [];
+  try {
+    _tasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(_tasks === null || _tasks === undefined){
+    _tasks = [];
+    }
+  } catch (e) {
+    _tasks = [];
+  }
+  return {
+    setTasks: function(tasks) {
+      debugger;
+      _tasks = tasks;
+      localStorage.setItem(LOCAL_STORAGE_KEY, angular.toJson(tasks));
+    },
+    getTasks: function() {
+      debugger;
+      return _tasks;
+    }
+  };
 });
 
-app.filter('byArchived', function() {
-    return function(tasks,archive) {
-      tasks = tasks.filter(function(task){
-        if(archive){
-          return task.archived;
-        } else {
-          return !task.archived;
-        }
-      });
-      return tasks;
-    }
-  });
-
-app.controller('ToDoCtrl', function($scope, $rootScope, $location) {
-  $scope.tasks = [];
+app.controller('ToDoCtrl', function($scope, $rootScope, $location, Tasks) {
+  $scope.tasks = Tasks.getTasks();
   $scope.newTaskName = '';
   $scope.archive = false;
 
-  $rootScope.$on("$routeChangeSuccess", function(event, next, current) {
-    if(next.$$route.originalPath === '/archive'){
-      $scope.archive = true;
-    } else {
-      $scope.archive = false;
-    }
-  });
+  $scope.$watch('tasks', function(newTasks) {
+    Tasks.setTasks(newTasks);
+  }, true);
 
   $scope.createNewTask = function() {
+    if ($scope.newTask.taskName.$invalid) {
+      return;
+    }
     $scope.tasks.push({
       name: $scope.newTaskName,
       done: false,
@@ -54,8 +75,25 @@ app.controller('ToDoCtrl', function($scope, $rootScope, $location) {
 
   $scope.archiveDoneTasks = function() {
     $scope.tasks = $scope.tasks.map(function(task) {
-      if(task.done) task.archived = true;
+      if (task.done) task.archived = true;
       return task;
     });
   };
+
+  $scope.notArchived = function(task){
+    return !task.archived;
+  }
+});
+
+app.controller('ArchiveCtrl', function($scope, $rootScope, $location, Tasks) {
+  $scope.tasks = Tasks.getTasks();
+  $scope.archive = true;
+
+  $scope.$watch('tasks', function(newTasks) {
+    Tasks.setTasks(newTasks);
+  }, true);
+
+  $scope.archived = function(task){
+    return task.archived;
+  }
 });
